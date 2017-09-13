920e0246facbe7b679b7d936076555953ffec92c
yinxueyuan <838476901@qq.com>
2017-09-13 21:06:03 +0800

    feat: init project

---
+++ lib/MPreview.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="vue-MPreview">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue-MPreview',
+
+  props: {},
+
+  data () {
+    return {}
+  },
+
+  computed: {},
+
+  methods: {},
+
+  mounted: function () {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+</style>
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {}
+  },
+  components: {},
+  methods: {},
+  mounted () {}
+}
+</script>
+
+<style lang="less">
+</style>
